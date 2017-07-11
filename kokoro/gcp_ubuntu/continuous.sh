eec08f60ba7bbfe963b0a5b6bc827762289075dd
Adele Zhou <adelez@google.com>
2017-07-10 17:33:13 -0700

    Kokoro GCP Ubuntu codelab continuous testing

--- kokoro/gcp_ubuntu/continuous.sh
+++ kokoro/gcp_ubuntu/continuous.sh
@@ -2,8 +2,12 @@
 
 # Fail on any error.
 set -e
-# Display commands to stderr.
+# Display commands being run.
 set -x
 
-cd git/kokoro-codelab-adelez
-./build.sh
+git clone https://github.com/leachim6/hello-world.git
+# Checkout the commit we trust
+cd hello-world
+git checkout 3bab02464b0fdc7c0e59cd39744ea432ec2baafa
+cd j
+javac Java.java
